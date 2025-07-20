49f7e6f208237a951b279e2075e0a8746809bc2e
Deepen Timalsina <deepentimalsina05@gmail.com>
2025-07-20 16:59:42 +0545

    udpate: project name change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,7 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  console.log('test log');
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
